e3b4208b43de4a7237907412d5649965fa3396cb
AmbartsumianL <ambartsumyan.lyudmila@gmail.com>
2025-04-14 01:47:42 +0300

    Update Task19_AQA

--- src/test/java/Task19_AQA/LogoSignUpTest.java
+++ src/test/java/Task19_AQA/LogoSignUpTest.java
@@ -13,15 +13,14 @@ public class LogoSignUpTest {
     WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
+    public void driverSet() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://guest:welcome2qauto@qauto.forstudy.space/");
     }
 
-    @Test(priority = 1, description = "Verify the visibility of the logo on the page")
-    public void testLogoDisplay() {
-       // driver.get("https://guest:welcome2qauto@qauto.forstudy.space/");
+    @Test(priority = 1, description = "Verify that logo is visible on the page")
+    public void logoTest() {
         try {
             WebElement logo = driver.findElement(By.xpath("//a[@class='header_logo']"));
             if (logo.isDisplayed()) {
@@ -34,20 +33,18 @@ public class LogoSignUpTest {
             Assert.fail("Test case1: Logo element was not found.");
         }
     }
-    @Test(priority = 2, description = "Verify the correct background color of the 'Sign up' button")
-    public void testSignUpButtonBackgroundColor() {
-        // driver.get("https://guest:welcome2qauto@qauto.forstudy.space/");
 
+    @Test(priority = 2, description = "Verify that background color of the 'Sign up' button is correct")
+    public void signUpTest() {
         try {
             WebElement signUpButton = driver.findElement(By.xpath("//button[contains(text(),'Sign up')]"));
 
             String backgroundColor = signUpButton.getCssValue("background-color");
-            //System.out.println("Background color of Sign up button: " + backgroundColor);
 
             if (backgroundColor.equals("rgba(2, 117, 216, 1)")) {
                 System.out.println("Test case2: Background color of Sign up button is correct (#0275d8)");
             } else {
-                System.out.println("Background color of Sign up button is incorrect.");
+                System.out.println("Test case2: Background color of Sign up button is incorrect.");
                 Assert.fail("Test case2: Background color of Sign up button is incorrect.");
             }
         } catch (Exception e) {
@@ -55,6 +52,7 @@ public class LogoSignUpTest {
             Assert.fail("Test case2: Sign up button element was not found.");
         }
     }
+
     @AfterMethod
     public void tearDown() {
         driver.quit();
