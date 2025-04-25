479d6b45bcfa10de6bd26808d8f5c7a0eeae5d97
AmbartsumianL <ambartsumyan.lyudmila@gmail.com>
2025-04-26 02:44:23 +0300

    Add Task22_frame

--- src/test/java/Task22_iFrame/FrameTest.java
+++ src/test/java/Task22_iFrame/FrameTest.java
@@ -15,6 +15,7 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Set;
 
 public class FrameTest {
     WebDriver driver;
@@ -34,21 +35,21 @@ public class FrameTest {
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
             WebElement iframe = wait.until(ExpectedConditions.presenceOfElementLocated(
                     By.cssSelector("iframe[src*='znjjC0Iw8Wc']")));
-            System.out.println("Step1: Found iframe → " + iframe.getAttribute("src"));
+            System.out.println("Found iframe → " + iframe.getAttribute("src"));
 
             driver.switchTo().frame(iframe);
-            System.out.println("Step2: Switched to iframe successfully");
+            //System.out.println("Switched to iframe successfully");
 
             WebElement title = wait.until(ExpectedConditions.visibilityOfElementLocated(
                     By.cssSelector("a.ytp-title-link")));
 
             String actualTitle = title.getText();
-            System.out.println("Step3: Found title → \"" + actualTitle + "\"");
+            System.out.println("Found title → \"" + actualTitle + "\"");
             String expectedTitle = "Як потрапити у майбутнє? Трансформація навчання.";
 
             Assert.assertEquals(actualTitle, expectedTitle, "Test case1: Title doesn’t equals to the expected result");
 
-            System.out.println("Test case1: YouTube iframe video title is correct");
+            //System.out.println("Test case1: YouTube iframe video title is correct");
             driver.switchTo().defaultContent();
 
         } catch (Exception e) {
@@ -56,28 +57,86 @@ public class FrameTest {
         }
     }
 
-    @Test(priority = 2, description = "Verify that footers social icons")
-    public void verifyFooterSocialIcons() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    @Test(priority = 2, description = "Verify social media links open correct pages and there are exactly 5 icons")
+    public void testSocialMediaLinks() {
         try {
+            driver.get("https://guest:welcome2qauto@qauto.forstudy.space/");
+            driver.manage().window().maximize();
+
             JavascriptExecutor js = (JavascriptExecutor) driver;
             js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
             System.out.println("Scrolled down to the footer");
-            //Thread.sleep(1000);
 
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
             List<WebElement> socialIcons = wait.until(ExpectedConditions
                     .presenceOfAllElementsLocatedBy(By.cssSelector("div.contacts_socials.socials a.socials_link")));
-            System.out.println("Found social icons = " + socialIcons.size());
-
             int actualResult = socialIcons.size();
             int expectedResult = 5;
-            System.out.println("Test case2: Social network block contains 5 items");
+            System.out.println("Found social icons = " + actualResult);
 
-            Assert.assertEquals(actualResult, expectedResult, "Test case2: Social network block doesn’t contain 5 items");
-        }catch (Exception e) {
-            Assert.fail("Test case2: Test failed at step: verifyFooterSocialIconsCount");
-        }}
+            Assert.assertEquals(actualResult, expectedResult,
+                    "Test case2: Social network block doesn’t contain 5 items");
 
+            boolean allUrlsCorrect = true;
+
+            for (int i = 0; i < socialIcons.size(); i++) {
+                try {
+                    List<WebElement> icons = driver.findElements(By.cssSelector("div.contacts_socials.socials a.socials_link"));
+                    WebElement icon = icons.get(i);
+
+                    String iconUrl = icon.getAttribute("href");
+                    // System.out.println("Clicking on icon");
+
+                    icon.click();
+                    // Thread.sleep(2000);
+
+                    Set<String> windows = driver.getWindowHandles();
+                    if (windows.size() == 1) {
+                        System.out.println("Test case2: New tab did not open after clicking ");
+                        allUrlsCorrect = false;
+                        continue;
+                    }
+
+                    String newTab = (String) windows.toArray()[1];
+                    driver.switchTo().window(newTab);
+
+                    String url = driver.getCurrentUrl();
+                    // System.out.println("Opened URL: " + url);
+
+                    if (url.contains("youtube.com") && url.contains("consent")) {
+                        System.out.println("YouTube consent page opened: " + url);
+                    } else if (url.contains("linkedin.com") && url.contains("authwall")) {
+                        System.out.println("LinkedIn authorization page opened: " + url);
+                    } else if (url.contains("facebook.com")) {
+                        System.out.println("Facebook URL opened: " + url);
+                    } else if (url.contains("twitter.com")) {
+                        System.out.println("Twitter URL opened: " + url);
+                    } else if (url.contains("instagram.com")) {
+                        System.out.println("Instagram URL opened: " + url);
+                    } else if (url.contains("t.me")) {
+                        System.out.println("Telegram URL opened: " + url);
+                    } else {
+                        System.out.println("Incorrect URL of Social network: " + url);
+                        allUrlsCorrect = false;
+                    }
+
+                    driver.close();
+                    driver.switchTo().window((String) windows.toArray()[0]);
+
+                } catch (Exception e) {
+                    System.out.println("Error while checking social icon ");
+                    allUrlsCorrect = false;
+                }
+            }
+
+            Assert.assertTrue(allUrlsCorrect,
+                    "Test case2: Incorrect url of Social network");
+
+        } catch (Exception e) {
+            Assert.fail("Test case2: Something went wrong");
+        }
+    }
 
 
     @AfterMethod
