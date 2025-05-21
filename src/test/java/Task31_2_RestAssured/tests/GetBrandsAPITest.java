f98485450ce425f126ee539b9e37f9c110ccc608
AmbartsumianL <ambartsumyan.lyudmila@gmail.com>
2025-05-21 21:55:11 +0300

    Add Task31_2_Rest-assured

--- src/test/java/Task31_2_RestAssured/tests/GetBrandsAPITest.java
+++ src/test/java/Task31_2_RestAssured/tests/GetBrandsAPITest.java
@@ -5,8 +5,6 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class GetBrandsAPITest {
