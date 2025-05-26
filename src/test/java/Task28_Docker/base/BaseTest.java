08bd520f736e1802f18f3676e55d23953c6abb67
AmbartsumianL <ambartsumyan.lyudmila@gmail.com>
2025-05-27 02:19:06 +0300

    Add Task28_docker

--- src/test/java/Task28_Docker/base/BaseTest.java
+++ src/test/java/Task28_Docker/base/BaseTest.java
@@ -1,11 +1,7 @@
 package Task28_Docker.base;
 
-import Task28_Docker.config.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
 
 public class BaseTest {
     protected WebDriver driver;
