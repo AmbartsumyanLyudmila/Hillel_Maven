681886ba5d4bda39f90db55dff47ea8878761c01
AmbartsumianL <ambartsumyan.lyudmila@gmail.com>
2025-05-30 23:19:06 +0300

    Add Task29_Jenkins

--- src/test/java/Task29_Jenkins_Allure/tests/AllureTestJenkins.java
+++ src/test/java/Task29_Jenkins_Allure/tests/AllureTestJenkins.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class AllureTest extends BaseTest {
+public class AllureTestJenkins extends BaseTest {
 
     @Epic("Garage Page")
     @Feature("Add Car")
